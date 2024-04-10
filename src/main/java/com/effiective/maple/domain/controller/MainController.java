dfa55e6dc1adcfd8c4329c6c2a6789c84c30db6a
JuneYoungDo <ehwnsdud1004@naver.com>
2024-04-10 20:33:59 +0900

    CI/CD 설정 및 구조 분리

---
+++ src/main/java/com/effiective/maple/domain/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.effiective.maple.domain.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    /**
+     * 메인 화면 가져오기
+     * [GET] /
+     */
+    @GetMapping("/")
+    public String getMainPage(){
+        return "index";
+    }
+}
