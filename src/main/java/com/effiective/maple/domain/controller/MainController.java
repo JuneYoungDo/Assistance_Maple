3d7904a819e062f150355b4fcee33512b7299348
JuneYoungDo <ehwnsdud1004@naver.com>
2024-04-11 15:58:06 +0900

    메인화면 수정

--- src/main/java/com/effiective/maple/domain/controller/MainController.java
+++ src/main/java/com/effiective/maple/domain/controller/MainController.java
@@ -1,9 +1,9 @@
 package com.effiective.maple.domain.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     /**
